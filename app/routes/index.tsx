38d1fc7c181af1a2b49a48ce102cbf4da51384de
amilin <moh.amilin@moladin.com>
2022-07-03 14:12:12 +0700

    import navbar

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,38 +1,11 @@
 import * as React from "react";
 
+import Navbar from '../components/Navbar';
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1 className="text-2xl font-extrabold tracking-tight text-gray-900">Welcome to Remix</h1>
-      <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
-      <ul>
-        <li className="font-medium text-gray-900">
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-          className="mt-2 text-sm text-gray-500"
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-      </div>
-     
+    <div>
+      <Navbar/>
     </div>
-  );
+  )
 }
